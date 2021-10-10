b7a430472a048ceb8a8dc9aa37b932c759643a18
Sunjida-Akter <92180941+Sunjida-Akter@users.noreply.github.com>
2021-10-10 21:05:14 +0600

    Update Arra.java

--- Arra.java
+++ Arra.java
@@ -1,1 +1,15 @@
 
+class Main { public static void main(String[] args) { 
+int[] numbers = {0,-1, 9, 1, 25, -7, -12, 0, -3, 8}; 
+int sum = 0;
+Double average;
+  for (int number: numbers) 
+  { 
+  sum += number; 
+} 
+  int arrayLength = numbers.length; 
+  average =((double)sum / (double)arrayLength); 
+System.out.println("Sum = " + sum); 
+System.out.println("Average = " + average);
+ }
+ }
